ee62de2402d7fa165c77c02927ecb5b5115f730e
tychonievichm <39139656+tychonievichm@users.noreply.github.com>
2018-07-12 14:56:01 -0400

    init files can be reset now

--- backend.py
+++ backend.py
@@ -41,7 +41,12 @@ class UI_Data:
         self.after = self.matrix.array @ self.before
 
 def _read_matrices_to_dict():
-    f = open("matrices.ini", "r")
+    '''Gets the list of matrices from the matrices.ini file.'''
+    try:
+        f = open("matrices.ini", "r")
+    except FileNotFoundError:
+        _renew_matrices_ini()
+        f = open("matrices.ini", "r")
     matrix_data = f.read()
     f.close()
     matrix_data = matrix_data.split("\n")
@@ -56,8 +61,24 @@ def _read_matrices_to_dict():
             # print("Added matrix " + name)
     return matrix_dict
 
+def _renew_matrices_ini():
+    '''Erases matrices.ini and replaces it with the default matrices.ini file.'''
+    f = open('matrices.ini', 'w+')
+    matrices_text = ["name:default\n0 1 -1 0\n\n",
+                     "name:contracting rotation\n0.3 0.8 -0.8 0.3\n\n",
+                     "name:expanding rotation\n0.9 0.7 -0.7 0.9\n\n",
+                     "name:real eigenvalues\n0.2 -1.8 -1.2 0.8\n"]
+    for s in matrices_text:
+        f.write(s)
+    f.close
+
 def _read_polygons_to_dict():
-    f = open("polygons.ini", "r")
+    '''Gets the list of polygons from the polygons.ini file.'''
+    try:
+        f = open("polygons.ini", "r")
+    except FileNotFoundError:
+        _renew_polygons_ini()
+        f = open("polygons.ini", "r")
     polygon_data = f.read()
     f.close()
     polygon_data = polygon_data.split("\n")
@@ -74,8 +95,26 @@ def _read_polygons_to_dict():
             polygon_dict[name] = Polygon(name, coord_list[0], coord_list[1])
     return polygon_dict
 
+
+                    
+def _renew_polygons_ini():
+    '''Erases polygons.ini and replaces it with the default polygon.ini file.'''
+    f = open('polygons.ini', 'w+')
+    polygons_text = ["name:rectangle\nx:0 0 2 2 0 0\ny:0 0 0 1 1 0\n\n",
+                     "name:line\nx:0 0 1.7 0\ny:0 0 0.3 0\n\n",
+                     "name:basis\nx:0 0 0 0 1 0\ny:0 0 1 0 0 0\n\n",
+                     "name:square\nx:0 0 1 1 0 0\ny:0 0 0 1 1 0\n\n",
+                     "name:dogegon\nx:1.3850 0.8711 1.0166 1.1751 1.7441 ",
+                     "2.0674 1.9413 1.8443 1.7861 1.7279 1.2818 1.1977 1.1686 ",
+                     "1.0490 1.0360 0.8679 0.8711\ny:1.4246 1.6176 1.8261 ",
+                     "1.6097 1.5595 1.4302 1.3378 1.3378 1.0422 1.3326 1.3378 ",
+                     "1.0106 1.3194 1.4012 1.5886 1.6176 1.6176\n\n"]
+    for s in polygons_text:
+        f.write(s)
+    f.close
+
 base_point = Base_Point(1, 2)
-ui = UI_Data("dogegon", "default", base_point)
+ui = UI_Data("dogegon", "expanding rotation", base_point)
 ax = plt.subplot(111)
 plt.plot(ui.before[0,], ui.before[1,], ui.after[0,], ui.after[1,])
 
